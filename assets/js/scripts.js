7629b79d4f46d7c721fcdfb93741da3ba37d399e
ian1zora <ianeest04@gmail.com>
2025-10-24 20:09:42 -0300

    egw

---
+++ assets/js/scripts.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    const preloader = document.getElementById("preloader");
+    setTimeout(() => {
+      preloader.classList.add("hidden");
+    }, 1000);
+  });
+  
